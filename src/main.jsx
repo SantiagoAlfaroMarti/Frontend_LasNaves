8399047c325bd11352e948cc9379b33e1f551f02
unknown <santiagoalfamar@gmail.com>
2024-10-10 11:33:11 +0200

    fix: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; 
-import { AuthProvider } from './contexts/AuthContext.jsx';
+import { AuthProvider } from './contexts/AuthContext';
 import App from './App.jsx';
 import './index.css';
 
